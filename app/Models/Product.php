840b7a931702d500a3c829d396ba9c6fc22f5832
HyperJAK <james.jakthesamething@gmail.com>
2024-04-05 16:23:11 +0300

    Fixed hasMany to hasOne and uncommented primary key

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,12 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    // protected $primaryKey = 'product_id'; //no need to rename it
-
     use HasFactory;
+    protected $primaryKey = 'product_id'; //no need to rename it
+
     protected $fillable = ['name','description','price','category','quantity','path1','path2','path3','path4'];
 
-//    public function stores(){
-//     return $this->hasMany(Store::class);
-//    }
+    //changed to hasOne for less complications that way we know that this product belongs to only this store
+    public function stores(){
+     return $this->hasOne(Store::class, 'store_id');
+    }
 }
