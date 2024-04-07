b21d4ce10c79353b09cdd86143e6a1409f42a2c4
HyperJAK <james.jakthesamething@gmail.com>
2024-04-07 18:52:30 +0300

    Updated Chat and Chatmessage Models and migration

--- app/Models/ChatMessage.php
+++ app/Models/ChatMessage.php
@@ -8,28 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class ChatMessage extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'message_id';
+
+
     protected $fillable = [
-        'content', 
-        'timestamp', 
-        'chat_id', 
+        'content',
+        'chat_id',
         'sender_id',
-        'recipient_id', 
+        'recipient_id',
     ];
 
-    
-    public function chat()
+
+    public function getchat()
     {
-        return $this->belongsTo(Chat::class);
+        return $this->belongsTo(Chat::class, 'chat_id', 'chat_id');
     }
 
-    public function sender()
+    public function getSender()
     {
-        return $this->belongsTo(User::class, 'sender_id');
+        return $this->belongsTo(User::class, 'sender_id', 'user_id');
     }
 
-    
-    public function recipient()
+
+    public function getRecipient()
     {
-        return $this->belongsTo(User::class, 'recipient_id');
+        return $this->belongsTo(User::class, 'recipient_id', 'user_id');
     }
 }
