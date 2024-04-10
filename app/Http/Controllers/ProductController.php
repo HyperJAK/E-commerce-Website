26377c2b04f39271d6fa48a46b7136002de2867a
Joe <joesalem789@gmail.com>
2024-04-11 01:30:52 +0300

    get products by name, by id by category by store now works with their routes

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 use App\Models\Product;
+use App\Models\Store;
 use Illuminate\Http\Request;
 
+// As a buyer i want to be able to add a product to my cart, to my Wishlist or normally browse it 
+// (Buyers can browse products, add to cart, and add to Wishlist.)
 class ProductController extends Controller
 {
-    public function getImages($id){
+    public function getProdImages($id){
         $obj= Product::find($id);
         $p1=$obj->path1;
         $p2=$obj->path2;
@@ -19,5 +22,46 @@ class ProductController extends Controller
             'p4'=>$p4
         ];
         return $full;
+    }
+    public function getProd($id){
+        $obj= Product::find($id);
+        if ($obj) { return $obj;
+        } else {
+           return response()->json(['message'=>'Product not found']);
+        }
+    }
+    public function getAllProd(){
+        $obj= Product::all();
+        if ($obj) { return $obj;
+        } else {
+           return response()->json(['message'=>'Products not found']);
+        }
+    }
+    public function getProdName($name){
+        $obj= Product::where('name','like',"%$name%")->get();
+        if ($obj->isNotEmpty()) { return $obj;
+        } else {
+           return response()->json(['message'=>'Products not found']);
+        }
+    }
+    public function getProdCategory($category){
+        $obj= Product::where('category',$category)->get();
+        if ($obj->isNotEmpty()) { return $obj;
+        } else {
+           return response()->json(['message'=>'No product found']);
+        }
+    }
+    public function getProdStore($store){
+        $storeCheck=Store::where('store_id',$store)->where('status','1')->get(); //not only exist but also verified by admin and active
+    if ($storeCheck->isNotEmpty()) {
+        $obj= Product::where('store_id',$store)->get();
+        if ($obj->isNotEmpty()) { 
+            return $obj;
+        } else {
+           return response()->json(['message'=>'No product found']);
+        }
+    }else {
+           return response()->json(['message'=>'Store does not exist or not verified yet']);
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('getProd/{id}',[ProductController::class,'getProd']);
+Route::get('getAllProd',[ProductController::class,'getAllProd']);
+Route::get('getProdName/{name}',[ProductController::class,'getProdName']);
+Route::get('getProdCategory/{category}',[ProductController::class,'getProdCategory']);
+Route::get('getProdStore/{store}',[ProductController::class,'getProdStore']);
+Route::get('getProdImages/{id}',[ProductController::class,'getProdImages']);
+
