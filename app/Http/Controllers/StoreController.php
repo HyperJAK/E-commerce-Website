0b612b676cec0f6af5d2ec8b5b93b8eb23e680a6
HyperJAK <james.jakthesamething@gmail.com>
2024-04-13 17:42:28 +0300

    Created All store CRUD operations that are thought of for now

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -2,9 +2,143 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Store;
 use Illuminate\Http\Request;
+use \Illuminate\Http\JsonResponse;
 
 class StoreController extends Controller
 {
-    //
+    public function getStore($id){
+        $store = Store::find($id);
+        if ($store) {
+            return $store;
+        } else {
+            return response()->json(['message'=>'Store not found']);
+        }
+    }
+
+    public function getAllStores(){
+        $stores = Store::all();
+        if ($stores->isNotEmpty()) {
+            return $stores;
+        } else {
+            return response()->json(['message'=>'Stores not found']);
+        }
+    }
+
+    public function getPendingStoresByUserId($userId)
+    {
+        $stores = Store::where('user_id', $userId)->where('status', '0')->get();
+        if ($stores->isNotEmpty()) {
+            return $stores;
+        } else {
+            return response()->json(['message'=>'Stores not found']);
+        }
+    }
+
+    public function getActiveStoresByUserId($userId)
+    {
+        $stores = Store::where('user_id', $userId)->where('status', '1')->get();
+        if ($stores->isNotEmpty()) {
+            return $stores;
+        } else {
+            return response()->json(['message'=>'Stores not found']);
+        }
+    }
+
+    public function addStore(Request $request): JsonResponse
+    {
+        Store::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'status' => $request->status,
+            'user_id' => $request->user_id,
+        ]);
+        return response()->json(["message" => "Store added successfully"]);
+    }
+
+    public function updateStore(Request $request, $id): JsonResponse
+    {
+        $store = Store::find($id);
+        if ($store) {
+            $store->update([
+                'name' => $request->name,
+                'description' => $request->description,
+                'status' => $request->status,
+                'user_id' => $request->user_id,
+            ]);
+            return response()->json(["message" => "Store updated successfully"]);
+        } else {
+            return response()->json(['message'=>'Store not found']);
+        }
+    }
+
+    public function updateStoreName(Request $request, $id): JsonResponse
+    {
+        $store = Store::find($id);
+        if ($store) {
+            $store->update([
+                'name' => $request->name,
+            ]);
+            return response()->json(["message" => "Store updated successfully"]);
+        } else {
+            return response()->json(['message'=>'Store not found']);
+        }
+    }
+
+    public function updateStoreDescription(Request $request, $id): JsonResponse
+    {
+        $store = Store::find($id);
+        if ($store) {
+            $store->update([
+                'description' => $request->description,
+            ]);
+            return response()->json(["message" => "Store updated successfully"]);
+        } else {
+            return response()->json(['message'=>'Store not found']);
+        }
+    }
+
+    public function updateStoreStatus(Request $request, $id): JsonResponse
+    {
+        $store = Store::find($id);
+        if ($store) {
+            $store->update([
+                'status' => $request->status,
+            ]);
+            return response()->json(["message" => "Store updated successfully"]);
+        } else {
+            return response()->json(['message'=>'Store not found']);
+        }
+    }
+
+    public function deleteStore($id): JsonResponse
+    {
+        $store = Store::find($id);
+        if ($store) {
+            $store->delete();
+            return response()->json(["message" => "Store deleted successfully"]);
+        } else {
+            return response()->json(['message'=>'Store not found']);
+        }
+    }
+
+    public function getStoreStatus($id){
+        $store = Store::find($id);
+        if ($store) {
+            return $store->status;
+        } else {
+            return response()->json(['message'=>'Store not found']);
+        }
+    }
+
+    public function getStoreCreator($id){
+        $store = Store::find($id);
+        if ($store) {
+            return $store->user_id;
+        } else {
+            return response()->json(['message'=>'Store not found']);
+        }
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,36 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\StoreController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Products routes
 Route::get('getProd/{id}',[ProductController::class,'getProd']);
 Route::get('getAllProd',[ProductController::class,'getAllProd']);
 Route::get('getProdName/{name}',[ProductController::class,'getProdName']);
 Route::get('getProdCategory/{category}',[ProductController::class,'getProdCategory']);
 Route::get('getProdStore/{store}',[ProductController::class,'getProdStore']);
 Route::get('getProdImages/{id}',[ProductController::class,'getProdImages']);
+
+
+
+
+
+//Stores routes
+// Read Routes
+Route::get('stores/{id}', [StoreController::class, 'getStore']); // Show a specific store
+Route::get('stores', [StoreController::class, 'getAllStores']); // Show a specific store
+Route::get('stores/getActiveByUserId/{userId}', [StoreController::class, 'getActiveStoresByUserId']); // Show all active stores for specific user
+Route::get('stores/getPendingByUserId/{userId}', [StoreController::class, 'getPendingStoresByUserId']); // Show all pending stores for specific user
+
+
+// Get Route for Store Status
+Route::get('stores/{id}/getStatus', [StoreController::class, 'getStoreStatus']);
+// Get Route for Store Creator
+Route::get('stores/{id}/getCreator', [StoreController::class, 'getStoreCreator']);
+
 
