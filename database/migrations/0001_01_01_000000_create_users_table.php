a2753ade06c9e63a4a65a5c1688d56276aa2b543
Charbel Azar <charbel_azar11@hotmail.com>
2024-04-14 17:50:22 +0300

    Update StoreController with enhanced logic for handling exceptions and validations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -22,21 +22,21 @@ return new class extends Migration
 
         });
 
-        /*Schema::create('password_reset_tokens', function (Blueprint $table) {
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
-        });*/
+        });
 
         //For now, its not needed
-        /*Schema::create('sessions', function (Blueprint $table) {
+        Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
             $table->foreignId('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
             $table->integer('last_activity')->index();
-        });*/
+        });
     }
 
     /**
@@ -45,7 +45,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-/*        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');*/
+       Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
     }
 };
