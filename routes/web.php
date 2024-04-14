74fc22ce1d14ae96433339438dfe07071090a3ff
Charbel Azar <charbel_azar11@hotmail.com>
2024-04-14 17:28:31 +0300

    Setup routes for stores, products, and orders management

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,40 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StoreController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\OrderController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Using individual routes instead of resource to be added later
+
+// Routes for Store management
+Route::get('stores', [StoreController::class, 'index'])->name('stores.index');
+Route::get('stores/create', [StoreController::class, 'create'])->name('stores.create');
+Route::post('stores', [StoreController::class, 'store'])->name('stores.store');
+Route::get('stores/{store}', [StoreController::class, 'show'])->name('stores.show');
+Route::get('stores/{store}/edit', [StoreController::class, 'edit'])->name('stores.edit');
+Route::put('stores/{store}', [StoreController::class, 'update'])->name('stores.update');
+Route::delete('stores/{store}', [StoreController::class, 'destroy'])->name('stores.destroy');
+
+// Routes for Product management within stores
+Route::get('products', [ProductController::class, 'index'])->name('products.index');
+Route::get('products/create', [ProductController::class, 'create'])->name('products.create');
+Route::post('products', [ProductController::class, 'store'])->name('products.store');
+Route::get('products/{product}', [ProductController::class, 'show'])->name('products.show');
+Route::get('products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+Route::put('products/{product}', [ProductController::class, 'update'])->name('products.update');
+Route::delete('products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+
+// Routes for Order management
+Route::get('orders', [OrderController::class, 'index'])->name('orders.index');
+Route::get('orders/create', [OrderController::class, 'create'])->name('orders.create');
+Route::post('orders', [OrderController::class, 'store'])->name('orders.store');
+Route::get('orders/{order}', [OrderController::class, 'show'])->name('orders.show');
+Route::get('orders/{order}/edit', [OrderController::class, 'edit'])->name('orders.edit');
+Route::put('orders/{order}', [OrderController::class, 'update'])->name('orders.update');
+Route::delete('orders/{order}', [OrderController::class, 'destroy'])->name('orders.destroy');
