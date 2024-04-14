294e15096be2124d8249fc69c9621eccb38c8fed
Joe-S11 <133280348+Joe-S11@users.noreply.github.com>
2024-04-14 21:49:47 +0300

    Dev merge into buyer interaction product (#18)
    * fixed store isnt getting added from POST request
    
    * Created All store CRUD operations that are thought of for now
    
    * Urgent fix Buyer product interaction joe (#15)
    
    * get products by name, by id by category by store now works with their routes
    
    * Add product was added
    
    * hotfix before product interaction commits (#13)
    
    * categories class created!
    
    * linked categories with the table products, other fixes too (#12)
    
    * post now works, added missing files, created api routes file and more
    
    * quick fix
    
    * edited table user, added more product controller functions like get not detailed products , update products, delete products (crud operation on product done)
    
    * added checks for some functions to make sure they exist before change, added deleteproduct api post
    
    * urgent fix for product controller plus basic wishlist create and get
    
    ---------
    
    Co-authored-by: HyperJAK <james.jakthesamething@gmail.com>
    Co-authored-by: JAK <63348015+HyperJAK@users.noreply.github.com>

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,16 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\StoreController;
 use App\Http\Controllers\WishlistController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//Products routes
 Route::get('getProd/{id}',[ProductController::class,'getProd']);
 Route::get('getAllProd',[ProductController::class,'getAllProd']);
 Route::get('getProdName/{name}',[ProductController::class,'getProdName']);
@@ -18,4 +22,18 @@ Route::get('getProdSmallCat/{category_id}',[ProductController::class,'getProdSma
 Route::get('getProdSmallStore/{store_id}',[ProductController::class,'getProdSmallStore']);
 Route::get('getProdSmallSearch/{search}',[ProductController::class,'getProdSmallSearch']);
 Route::get('getWishlist/{user_id}',[WishlistController::class,'getWishlist']);
+
+//Stores routes
+// Read Routes
+Route::get('stores/{id}', [StoreController::class, 'getStore']); // Show a specific store
+Route::get('stores', [StoreController::class, 'getAllStores']); // Show a specific store
+Route::get('stores/getActiveByUserId/{userId}', [StoreController::class, 'getActiveStoresByUserId']); // Show all active stores for specific user
+Route::get('stores/getPendingByUserId/{userId}', [StoreController::class, 'getPendingStoresByUserId']); // Show all pending stores for specific user
+
+
+// Get Route for Store Status
+Route::get('stores/{id}/getStatus', [StoreController::class, 'getStoreStatus']);
+// Get Route for Store Creator
+Route::get('stores/{id}/getCreator', [StoreController::class, 'getStoreCreator']);
+
 
