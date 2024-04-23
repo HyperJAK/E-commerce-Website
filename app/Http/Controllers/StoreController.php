8d9ebb57464b998184c56d056811a3123a38a89e
HyperJAK <james.jakthesamething@gmail.com>
2024-04-23 21:49:06 +0300

    Fixed a sort function to get one category and all its shops and added a duplicate checker

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -57,27 +57,49 @@ class StoreController extends Controller
 
     public function SortStoresByCategory($categoryId)
     {
-        $categories = CategoryForStores::where('category_id', $categoryId);
+        $categories = CategoryForStores::where('category_id', $categoryId)->get();
+        $cats = CategoryForStores::all()->unique();
         $groupedData = [];
+        $groupedCats = [];
 
-        foreach ($categories as $category) {
-            $categoryId = $category->category_id;
 
-            if (!isset($groupedData[$categoryId])) {
-                $groupedData[$categoryId] = [
-                    'id' => $category->id,
+        foreach ($categories as $category){
+            $catId = $category->category_id;
+
+            if (!isset($groupedData[$catId])) {
+                $groupedData[$catId] = [
+                    'id' => $catId,
                     'category' => null,
                     'stores' => [],
                 ];
 
-                $groupedData[$categoryId]['category'] = Category::where('category_id', $category->category_id)->first();
+
+            }
+
+            $groupedData[$catId]['category'] = Category::where('category_id', $catId)->first();
+
+            $store = Store::where('store_id', $category->store_id)->first();
+
+
+            $storesCollection = collect($groupedData[$catId]['stores']);
+
+            if (!$storesCollection->contains('store_id', $store->store_id)) {
+                $groupedData[$catId]['stores'][] = $store;
             }
 
 
-            $groupedData[$categoryId]['stores'][] = Store::where('store_id', $category->store_id)->first();
         }
 
-        return view('stores')->with('categories', $groupedData);/*$groupedData*/
+
+        foreach ($cats as $cat) {
+            $categoryId = $cat->category_id;
+
+            $groupedCats[$categoryId] = Category::find($cat->category_id);
+
+        }
+
+
+        return view('stores')->with('cats', $groupedCats)->with('categories', $groupedData);/*$groupedData*/
     }
 
     public function getStoresByCategory()
@@ -85,7 +107,6 @@ class StoreController extends Controller
         $categories = CategoryForStores::all();
         $groupedData = [];
         $groupedCats = [];
-        $cats=CategoryForStores::all()->unique();
 
         foreach ($categories as $category) {
             $categoryId = $category->category_id;
@@ -103,7 +124,14 @@ class StoreController extends Controller
 
             $groupedCats[$categoryId] = Category::where('category_id', $category->category_id)->first();
 
-            $groupedData[$categoryId]['stores'][] = Store::where('store_id', $category->store_id)->first();
+            $store = Store::where('store_id', $category->store_id)->first();
+
+
+            $storesCollection = collect($groupedData[$categoryId]['stores']);
+
+            if (!$storesCollection->contains('store_id', $store->store_id)) {
+                $groupedData[$categoryId]['stores'][] = $store;
+            }
 
         }
 
