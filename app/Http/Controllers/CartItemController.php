53f0cf0a0403eaca7aaf1b59c57cf4df452241b0
HyperJAK <james.jakthesamething@gmail.com>
2024-04-26 02:25:59 +0300

    modified function to check if a user has an open cart and if not it creates one with status 0, later on when user places order on his cart, his cart items are transfered to order table and his cart status is set to 1

--- app/Http/Controllers/CartItemController.php
+++ app/Http/Controllers/CartItemController.php
@@ -2,29 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
 use App\Models\CartItem;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use PhpParser\Node\Expr\Cast\Double;
 
 class CartItemController extends Controller
 {
     public function AddCartItem(Request $request){
         $request->validate([
-           'cart_id'=>'required|exists:carts,cart_id|numeric',
+/*           'cart_id'=>'required|exists:carts,cart_id|numeric',*/
            'product_id'=>'required|exists:products,product_id|numeric',
            'quantity'=>'required|numeric',
             ],[
                 'exists'=>'Cart Id or Product Id do not exist',
             ]);
             $price= Product::select('price')->where('product_id',$request->product_id)->get();
-        CartItem::create([
-            'price'=> $price->pluck('price')->first(),
-            'quantity'=> $request->quantity,
-            'product_id'=> $request->product_id,
-            'cart_id'=>$request->cart_id,
-        ]);
-        return response()->json(['message'=>"CartItem added successfully"],200);
+
+            //to test if theres an open cart
+            $availableCartTest = Cart::select('cart_id')->where('status', 0)->get();
+
+            if(!$availableCartTest->isNotEmpty()){
+                $cartController = new CartController();
+                $cartController->AddCart($request);
+            }
+
+            //after cart is created we get it
+        $availableCart = Cart::select('cart_id')->where('status', 0)->get();
+
+            if($availableCart->isNotEmpty()){
+                CartItem::create([
+                    'price'=> $price->pluck('price')->first(),
+                    'quantity'=> $request->quantity,
+                    'product_id'=> $request->product_id,
+                    'cart_id'=>$availableCart[0]->cart_id,
+                ]);
+            }
+
+        return redirect()->route('getProd',['id'=>$request->product_id]);
 }
 public function EditCartItemQtt(Request $request){
     $request->validate([
@@ -34,7 +51,7 @@ public function EditCartItemQtt(Request $request){
     $obj= CartItem::find($request->cartItem_id);
     if($request->quantity>=0){
         $obj->quantity = $request->quantity;
-        $obj->save();        
+        $obj->save();
     return response()->json(["message"=>"CartItem quantity edited successfully"]);
     }else{
         return response()->json(["message"=>"CartItem quantity cannot be <0"]);
@@ -43,18 +60,33 @@ public function EditCartItemQtt(Request $request){
 public function getCartItem($cart_id) {
     $cartItem = CartItem::where('cart_id',$cart_id)->get();
     if ($cartItem->isNotEmpty()) {
-      return $cartItem;  
+      return $cartItem;
     }else{
     return response()->json(['message' => 'cartItems not found or this user has no items in his cart'], 404);
     }
 }
 public function DeleteCartItem($cartItem_id){
     $obj= CartItem::find($cartItem_id);
+    $product_id = $obj->product_id;
+    $wished = false;
+
+
+
+    if($obj){
+        $checkIfWishlisted= Product::find($obj->product_id);
+
+        if(count($checkIfWishlisted->getUserStatus(Auth::id()))>0){
+            $wished=true;
+        }else{
+            $wished=false;
+        }
+    }
+
     if ($obj) {
-        $obj->delete();        
-    return response()->json(["message"=>"CartItem deleted successfully"]);
+        $obj->delete();
+    return redirect()->route('getProd',['id'=>$product_id])->with('wished', $wished);
     } else {
-    return response()->json(['message'=>'CartItem does not exist or delete CartItem failed']);
+    return redirect()->route('getProd',['id'=>$product_id])->with('wished', $wished);
 }
     }
 }
