3f905b1f00d6ae1acedde8b55dec8df54d43d05f
HyperJAK <james.jakthesamething@gmail.com>
2024-04-28 01:09:37 +0300

    Fixed store database table seeder

--- database/seeders/StoresTableSeeder.php
+++ database/seeders/StoresTableSeeder.php
@@ -19,28 +19,28 @@ class StoresTableSeeder extends Seeder
                 'description' => 'Description of Store 1',
                 'status' => false, // or false based on your requirements
                 'user_id' => 1, // Assuming you have users already seeded
-                'images/storeImages/1/image1.png',
+                'image' => 'images/storeImages/1/image1.png',
             ],
             [
                 'name' => 'Store 2',
                 'description' => 'Description of Store 2',
                 'status' => true, // or true based on your requirements
                 'user_id' => 2, // Assuming you have users already seeded
-                'images/storeImages/2/image1.png'
+                'image' => 'images/storeImages/2/image1.png'
             ],
             [
                 'name' => 'Store 3',
                 'description' => 'Description of Store 3',
                 'status' => true, // or true based on your requirements
                 'user_id' => 1, // Assuming you have users already seeded
-                'images/storeImages/1/image2.png'
+                'image' => 'images/storeImages/1/image2.png'
             ],
             [
                 'name' => 'Store 4',
                 'description' => 'Description of Store 4',
                 'status' => true, // or true based on your requirements
                 'user_id' => 2, // Assuming you have users already seeded
-                'images/storeImages/2/image2.png'
+                'image' => 'images/storeImages/2/image2.png'
             ],
             // Add more stores as needed
         ]);
