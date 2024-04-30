6c50b3aca63608728f91c99999de21f58533accb
Charbel Azar <charbel_azar11@hotmail.com>
2024-04-30 13:22:49 +0300

    creating tables and models

---
+++ resources/views/indexx.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content ="IE=edge">
+    <meta name="viewport" content="width=device-width,
+          initial-scale=1.0">
+    <title>Laravel Online</title>
+    <link rel="stylesheet" href="/css1/style1.css">
+</head>
+<body>
+<div class="app">
+    <header>
+        <h1>Let's Talk! </h1>
+                <input type="text" name="username" id="username" placeholder="Please enter a username..."/>
+    </header>
+    <div id="messages"></div>
+    <form id="message_form">
+        <input type="text" name="message" id="message_input" placeholder="write a message..."/>
+        <button type="submit" id="message_send">Send Message</button>
+    </form>
+
+</div>
+<script src="/js/app.js"></script>
+
+</body>
+</head>
+
+</html>
