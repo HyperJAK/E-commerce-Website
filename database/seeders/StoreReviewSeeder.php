0c03528d1bf5af48e7b2de364d9c0f2ca24b9742
HyperJAK <james.jakthesamething@gmail.com>
2024-05-10 01:26:25 +0300

    Added Product and store review tables and models and seeders

---
+++ database/seeders/StoreReviewSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use App\Models\ProductReviews;
+use App\Models\Store;
+use App\Models\StoreReviews;
+use App\Models\User;
+use Faker\Factory as Faker;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class StoreReviewSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+
+        $userIds = User::pluck('user_id')->toArray();
+
+        $storeIds = Store::pluck('store_id')->toArray();
+
+        foreach (range(1, 50) as $index) {
+            $review = new StoreReviews();
+            $review->content = $faker->paragraph();
+            $review->rating = $faker->numberBetween(1, 5);
+            $review->user_id = $faker->randomElement($userIds);
+            $review->store_id = $faker->randomElement($storeIds);
+            $review->save();
+        }
+    }
+}
