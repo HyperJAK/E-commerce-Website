d832c6629acf3f14840873f4d5d9906a754fc301
HyperJAK <james.jakthesamething@gmail.com>
2024-05-10 01:40:21 +0300

    Created controller functions to get information of reviews and created routes for them

--- app/Http/Controllers/ProductReviewController.php
+++ app/Http/Controllers/ProductReviewController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductReviewController extends Controller
 {
-    //
+    public function getProductReviews(Request $request)
+    {
+        $productReviews = DB::table('product_reviews')->join('users', 'product_reviews.user_id', '=', 'users.user_id')->select('product_reviews.*', 'users.username as user_name', 'users.email as user_email')->where('product_reviews.product_id', $request->productId)->get();
+        return response()->json(['product_reviews' => $productReviews]);
+    }
 }
